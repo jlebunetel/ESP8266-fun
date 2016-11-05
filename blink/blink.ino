91e9b1c61f606a5f747e18748703b3bdfb784c26
Julien Lebunetel <julien@lebunetel.com>
2016-11-05 19:49:58 +0100

    blink sketch creation

---
+++ blink/blink.ino
@@ -0,0 +1,17 @@
+/*
+blink 
+turn on and off a led on the pin D7 of the LoLin ESP-12E NodeMcu V3
+*/
+
+int led = 13; // D7 = GPIO13
+
+void setup() {
+    pinMode(led, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+    delay(1000);
+}
