41017e185d515bee857ef4f5098d18b660d1d15a
Julien Lebunetel <julien@lebunetel.com>
2017-04-05 14:10:43 +0200

    add LED_BUILTIN

--- blink/blink.ino
+++ blink/blink.ino
@@ -3,7 +3,8 @@ blink
 turn on and off a led on the pin D7 of the LoLin ESP-12E NodeMcu V3
 */
 
-int led = 13; // D7 = GPIO13
+//int led = 13; // D7 = GPIO13
+int led = LED_BUILTIN;
 
 void setup() {
     pinMode(led, OUTPUT);
